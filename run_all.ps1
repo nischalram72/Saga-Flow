7f8a187a4303fb1a24a0e2030b9f37bc286024ce
nischalram72 <nischalram72@gmail.com>
2026-09-27 00:11:29 +0530

    Daily project progress update

---
+++ run_all.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Starting Sagaflow Services..."
+
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$job1 = Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$scriptPath\order-service'; .\venv\Scripts\Activate.ps1; uvicorn app.main:app --reload --port 8001" -PassThru
+$job2 = Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$scriptPath\inventory-service'; .\venv\Scripts\Activate.ps1; uvicorn app.main:app --reload --port 8002" -PassThru
+$job3 = Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$scriptPath\payment-service'; .\venv\Scripts\Activate.ps1; uvicorn app.main:app --reload --port 8003" -PassThru
+$job4 = Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$scriptPath\orchestrator-service'; .\venv\Scripts\Activate.ps1; uvicorn app.main:app --reload --port 8004" -PassThru
+
+$job5 = Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$scriptPath\frontend'; npm run dev" -PassThru
+
+Write-Host "All services started in new windows!"
